b687d341ec50507d7328fbdda6eafb9365b836a6
zeretkk <kirill03192@gmail.com>
2024-03-20 12:13:19 +0300

    remove redux persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,4 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import createWebStorage from 'redux-persist/lib/storage/createWebStorage'
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER
-} from 'redux-persist'
 
 import themeAppReducer from './slices/theme-slice'
 
@@ -17,42 +6,12 @@ const rootReducer = combineReducers({
   theme: themeAppReducer
 })
 
-const createNoopStorage = () => {
-  return {
-    getItem() {
-      return Promise.resolve(null)
-    },
-    setItem(_key: string, value: string) {
-      return Promise.resolve(value)
-    },
-    removeItem() {
-      return Promise.resolve()
-    }
-  }
+export const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer
+  })
 }
 
-const storage = typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage()
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: [],
-  whitelist: ['theme']
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-      }
-    }).concat()
-})
-
-export const persistor = persistStore(store)
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
